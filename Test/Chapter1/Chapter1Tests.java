3a9236a883da65a95fe1b33d6029f3d3aaac29e2
KevinAu <thekevinau@gmail.com>
2022-12-26 12:51:16 +1000

    Init q1, and testing. Refreshed cache

---
+++ Test/Chapter1/Chapter1Tests.java
@@ -0,0 +1,23 @@
+package Chapter1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Chapter1Tests {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void basicTestQ1_01() throws Exception {
+        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique("string");
+
+    }
+
+}
---
+++ src/Chapter1/Q1_01_Is_Unique.java
@@ -0,0 +1,16 @@
+package Chapter1;
+
+
+/**
+ * This function assumes
+ */
+public class Q1_01_Is_Unique {
+
+    String string;
+    boolean[] charBinaryTable = new boolean[128];
+
+    public Q1_01_Is_Unique(String string) {
+        this.string = string;
+    }
+
+}
