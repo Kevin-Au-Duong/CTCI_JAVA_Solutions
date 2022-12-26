a3ef58fd1da64d037e7a08af3cfe16037df0b8e0
kevinauduong <thekevinau@gmail.com>
2022-12-26 14:56:28 +1000

    Finished Q1.1

--- Test/Chapter1/Chapter1Tests.java
+++ Test/Chapter1/Chapter1Tests.java
@@ -1,6 +1,7 @@
 package Chapter1;
 
 import org.junit.After;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,9 +16,45 @@ public class Chapter1Tests {
     }
 
     @Test
-    public void basicTestQ1_01() throws Exception {
-        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique("string");
+    public void basicTestQ1_01_1() throws Exception {
+        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique("String");
+        assertTrue(q101IsUnique.checkUniqueString());
+    }
 
+    @Test
+    public void basicTestQ1_01_2() throws Exception {
+        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique("No Nut November");
+        assertFalse(q101IsUnique.checkUniqueString());
+    }
+
+    @Test
+    public void uppercaseTestQ1_01_1() throws Exception {
+        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique("HeLlo");
+        assertTrue(q101IsUnique.checkUniqueString());
+    }
+
+    @Test
+    public void numberTestQ1_01_1() throws Exception {
+        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique("a1");
+        assertTrue(q101IsUnique.checkUniqueString());
+    }
+
+    @Test
+    public void emptyTestQ1_01_1() throws Exception {
+        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique("");
+        assertTrue(q101IsUnique.checkUniqueString());
+    }
+
+    @Test
+    public void doubleSpaceTestQ1_01_1() throws Exception {
+        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique("  ");
+        assertFalse(q101IsUnique.checkUniqueString());
+    }
+
+    @Test
+    public void oneCharTestQ1_01_1() throws Exception {
+        Q1_01_Is_Unique q101IsUnique = new Q1_01_Is_Unique(" ");
+        assertTrue(q101IsUnique.checkUniqueString());
     }
 
 }
--- src/Chapter1/Q1_01_Is_Unique.java
+++ src/Chapter1/Q1_01_Is_Unique.java
@@ -1,16 +1,32 @@
 package Chapter1;
-
+import java.util.*;
 
 /**
- * This function assumes
+ * This solution assumes that every character in the provided string
+ * is an ascii character (has a unique ascii code)
  */
 public class Q1_01_Is_Unique {
 
     String string;
     boolean[] charBinaryTable = new boolean[128];
 
+
     public Q1_01_Is_Unique(String string) {
         this.string = string;
+        Arrays.fill(charBinaryTable, Boolean.FALSE);
+    }
+
+    public boolean checkUniqueString() {
+        for (int i = 0; i < string.length(); i++) {
+            int ascii = string.charAt(i);
+            if (!charBinaryTable[ascii]) {
+                charBinaryTable[ascii] = true;
+            } else {
+                return false;
+            }
+
+        }
+        return true;
     }
 
 }
