c47448fd3097617e4573d3aab02606c7c7f356d5
kevinauduong <thekevinau@gmail.com>
2023-01-01 16:17:06 +1000

    Init Q1.4.

--- src/Chapter1/Q1_04_Palindrome_Permutation.java
+++ src/Chapter1/Q1_04_Palindrome_Permutation.java
@@ -1,4 +1,13 @@
 package Chapter1;
 
+/**
+ * Question 1.4
+ * Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
+ * A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+ * is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+ *
+ * 
+ */
+
 public class Q1_04_Palindrome_Permutation {
 }
